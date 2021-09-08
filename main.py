1af4f47eeb84e3b3bab25908328671d5fb5e2a17
Asidrus <asidrus@mail.ru>
2021-09-08 14:38:10 +0300

    v 0.2.2

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import asyncio
-import logging
-from threading import Thread
-
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import exceptions, executor
-from server import *
+from TelegramBot import TelegramBot
+import asyncio
+from server import run_server
 
 API_TOKEN = "1924016224:AAF4TufT_s-WLu5a1WbXOl04NL9Wfq0MpEI"
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger('broadcast')
-bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = TelegramBot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
@@ -27,64 +23,10 @@ async def get_text_messages(msg: types.Message):
         await msg.answer('Не понимаю, что это значит.')
 
 
-def get_users():
-    """
-    Return users list
-
-    In this example returns some random ID's
-    """
-    yield from (936364717, )
+# loop = asyncio.new_event_loop()
+# loop.create_task(run_server("localhost", 1234))
 
 
-async def send_message(user_id: int, text: str, disable_notification: bool = False) -> bool:
-    """
-    Safe messages sender
-
-    :param user_id:
-    :param text:
-    :param disable_notification:
-    :return:
-    """
-    try:
-        await bot.send_message(user_id, text, disable_notification=disable_notification)
-    except exceptions.BotBlocked:
-        log.error(f"Target [ID:{user_id}]: blocked by user")
-    except exceptions.ChatNotFound:
-        log.error(f"Target [ID:{user_id}]: invalid user ID")
-    except exceptions.RetryAfter as e:
-        log.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
-        await asyncio.sleep(e.timeout)
-        return await send_message(user_id, text)  # Recursive call
-    except exceptions.UserDeactivated:
-        log.error(f"Target [ID:{user_id}]: user is deactivated")
-    except exceptions.TelegramAPIError:
-        log.exception(f"Target [ID:{user_id}]: failed")
-    else:
-        log.info(f"Target [ID:{user_id}]: success")
-        return True
-    return False
-
-
-async def broadcaster(msg) -> int:
-    """
-    Simple broadcaster
-
-    :return: Count of messages
-    """
-    count = 0
-    try:
-        for user_id in get_users():
-            if await send_message(user_id, f'<a>{msg.decode()}!</a>'):
-                count += 1
-            await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
-    finally:
-        log.info(f"{count} messages successful sent.")
-
-    return count
-
-
-if __name__ == '__main__':
-    # asyncio.run(run_server('127.0.0.1', 1234))
-    serv = Thread(target=asyncio.run, args=(run_server('127.0.0.1', 1234),))
-    serv.start()
-    executor.start_polling(dp)
+# ex = executor.Executor(dp, loop=loop)
+ex = executor.Executor(dp)
+ex.start_polling()
--- server.py
+++ server.py
@@ -1,107 +1,49 @@
 import asyncio
-from aiogram import Bot
-from aiogram.utils import exceptions
-import logging
 
 counter = 0
 
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger('broadcast')
+
+async def serve_client(reader, writer):
+    global counter
+    cid = counter
+    counter += 1  # Потоко-безопасно, так все выполняется в одном потоке
+    print(f'Client #{cid} connected')
+
+    request = await read_request(reader)
+    if request is None:
+        print(f'Client #{cid} unexpectedly disconnected')
+    else:
+        response = await handle_request(request)
+        await write_response(writer, response, cid)
 
 
-class ServerBot(Bot):
+async def handle_request(request):
+    await asyncio.sleep(5)
+    return request[::-1]
 
-    def run(self, host, port):
-        asyncio.run(self.run_server(host, port))
 
-    async def serve_client(self, reader, writer):
-        global counter
-        cid = counter
-        counter += 1  # Потоко-безопасно, так все выполняется в одном потоке
-        print(f'Client #{cid} connected')
+async def read_request(reader, delimiter=b'!'):
+    request = bytearray()
+    while True:
+        chunk = await reader.read(4)
+        if not chunk:
+            # Клиент преждевременно отключился.
+            break
+        request += chunk
+        if delimiter in request:
+            return request
+    return None
 
-        request = await self.read_request(reader)
-        if request is None:
-            print(f'Client #{cid} unexpectedly disconnected')
-        else:
-            response = await self.handle_request(request)
-            await self.write_response(writer, response, cid)
 
-    async def read_request(self, reader, delimiter=b'!'):
-        request = bytearray()
-        while True:
-            chunk = await reader.read(4)
-            if not chunk:
-                # Клиент преждевременно отключился.
-                break
-            request += chunk
-            if delimiter in request:
-                return request
-        return None
+async def write_response(writer, response, cid):
+    # await Bot.broadcaster(response)
+    print(response)
+    writer.write(response)
+    await writer.drain()
+    writer.close()
+    print(f'Client #{cid} has been served')
 
-    async def handle_request(self, request):
-        await asyncio.sleep(5)
-        return request[::-1]
 
-    async def write_response(self, writer, response, cid):
-        await self.broadcaster(response)
-        writer.write(response)
-        await writer.drain()
-        writer.close()
-        print(f'Client #{cid} has been served')
-
-    async def run_server(self, host, port):
-        server = await asyncio.start_server(self.serve_client, host, port)
-        await server.serve_forever()
-
-    def get_users(self):
-        """
-        Return users list
-
-        In this example returns some random ID's
-        """
-        yield from (936364717,)
-
-    async def _send_message(self, user_id: int, text: str, disable_notification: bool = False) -> bool:
-        """
-        Safe messages sender
-
-        :param user_id:
-        :param text:
-        :param disable_notification:
-        :return:
-        """
-        try:
-            await self.send_message(user_id, text, disable_notification=disable_notification)
-        except exceptions.BotBlocked:
-            log.error(f"Target [ID:{user_id}]: blocked by user")
-        except exceptions.ChatNotFound:
-            log.error(f"Target [ID:{user_id}]: invalid user ID")
-        except exceptions.RetryAfter as e:
-            log.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
-            await asyncio.sleep(e.timeout)
-            return await self._send_message(user_id, text)  # Recursive call
-        except exceptions.UserDeactivated:
-            log.error(f"Target [ID:{user_id}]: user is deactivated")
-        except exceptions.TelegramAPIError:
-            log.exception(f"Target [ID:{user_id}]: failed")
-        else:
-            log.info(f"Target [ID:{user_id}]: success")
-            return True
-        return False
-
-    async def broadcaster(self, msg) -> int:
-        """
-        Simple broadcaster
-
-        :return: Count of messages
-        """
-        count = 0
-        try:
-            for user_id in self.get_users():
-                if await self._send_message(user_id, f'<a>{msg.decode()}!</a>'):
-                    count += 1
-                await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
-        finally:
-            log.info(f"{count} messages successful sent.")
-        return count
+async def run_server(host, port):
+    server = await asyncio.start_server(serve_client, host, port)
+    await server.serve_forever()
