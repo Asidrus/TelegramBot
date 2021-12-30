e7b09edf69cffce99135d8e5d756efdf1f19e3a7
Tatyana636 <58527252+Tatyana636@users.noreply.github.com>
2021-12-30 16:39:32 +0300

    add columns for function
    function add_user

--- libs/database.py
+++ libs/database.py
@@ -46,8 +46,8 @@ class DataBase:
             Values({data["id"]},{data["is_bot"]},'{data["first_name"]}', '{data["last_name"]}', '3', '{data["language_code"]}')"""
         )
         await connection.fetch(
-            f"""INSERT into subscribes (uid, debug, from_users, result_tests) 
-            Values({data["id"]}, '0', '0', '0')"""
+            f"""INSERT into subscribes (uid, debug, from_users, res_all_tests, rt_penta, rt_psy, rt_mult, rt_spo)
+            Values({data["id"]}, '0', '0', '0', '0', '0', '0', '0')"""
         )
 
     @db_connection(**cred)
